0286b2c87ce0581baa0e25888f9aeabce4e1fb4b
Thomas <tbaglioni@hotmail.com>
2019-04-09 21:36:29 +0200

    Created a webserver that reads a file

--- 02 Second session/demo_readfile.js
+++ 02 Second session/demo_readfile.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+   console.log(req);
+   fs.writeFile("./request.txt",function(err, data){});
+  fs.readFile('demofile2.html', function(err, data) {
+  // console.log(err);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
