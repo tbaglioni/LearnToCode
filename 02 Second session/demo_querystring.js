2e249b94f8c7f41957a1e3fc1822159dbbffa20e
Brian Andersen <brianmanden@gmail.com>
2019-04-11 20:14:14 +0200

    Added desc + link til demo_querystring.js

--- 02 Second session/demo_querystring.js
+++ 02 Second session/demo_querystring.js
@@ -5,5 +5,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   var txt = q.year + " " + q.month;
+  txt += "<br/> Go to the following URL and play with the URL parameters";
+  txt += "<br/> <a href='http://localhost:8080/?year=someYear&month=someMonth'>http://localhost:8080/?year=someYear&month=someMonth</a>";
   res.end(txt);
 }).listen(8080);
