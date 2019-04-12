1b1d5d88421f59f34dcde888ac14445ce0df461a
Brian Andersen <brianmanden@gmail.com>
2019-04-12 10:26:00 +0200

    Thomas has used Brians computer for editing demo_querystring.js and made a new code to it and learned to code a little about date month and year

--- 02 Second session/demo_querystring.js
+++ 02 Second session/demo_querystring.js
@@ -4,8 +4,8 @@ var url = require('url');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
+  var txt = q.date + "/" + q.month + " - " + q.year;
   txt += "<br/> Go to the following URL and play with the URL parameters";
-  txt += "<br/> <a href='http://localhost:8080/?year=someYear&month=someMonth'>http://localhost:8080/?year=someYear&month=someMonth</a>";
+  txt += "<br/> <a href='http://localhost:8080/?date=12&month=april&year=2019'>http://localhost:8080/?date=12&month=april&year=2019</a>";
   res.end(txt);
 }).listen(8080);
