c78dffbce6dd32d8bafcde96eb84c9b0210b0399
Thomas <tbaglioni@hotmail.com>
2019-04-22 11:25:53 +0200

    Morning Code stolen from Brianmanden lets see what it does, I know it isnt my kind of server but maybe I can use some of it

--- 04 Fourth session/webserver.js
+++ 04 Fourth session/webserver.js
@@ -1,3 +1,23 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+
+http 
+    .createServer(function (req, res) {
+        const query = url.parse(req.url, true).query;
+        const content = query.content;
+        const textFile = './textFile.txt';
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        if (content) {
+            fs.appendFileSync(textFile, content + '/r/n', 'utf8');
+
+        }
+
+        const fileContents = fs.readFileSync(textFile, 'utf8');
+
+        res.write(fileContents);
+        res.end();
+    })
+    .listen(8080);
