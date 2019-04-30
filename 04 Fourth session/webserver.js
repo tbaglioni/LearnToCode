6c1ef4db81d3341e0e6fef0a9bacaada1fbe08b6
Brian Andersen <brianmanden@gmail.com>
2019-04-30 22:14:53 +0200

    Add some output elements

--- 04 Fourth session/webserver.js
+++ 04 Fourth session/webserver.js
@@ -1,11 +1,11 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+const port = 8080;
 
 http.createServer(function (req, res) {
         const query = url.parse(req.url, true).query;
         const content = query.content;
-        console.log(content);
         const nyurlparam = query.nyurlparam;
         const elephant = query.elefant;
 
@@ -13,15 +13,20 @@ http.createServer(function (req, res) {
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
 
-        // if (content) {
-        //     fs.appendFileSync(textFile, content + '/r/n', 'utf8');
-        //     fs.appendFileSync(textFile, nyurlparam + '/r/n', 'utf8');
-        //     fs.appendFileSync(textFile, elephant + '/r/n', 'utf8');
-        // }
+        if (content) {
+            fs.appendFileSync(textFile, content + '\r\n', 'utf8');
+            fs.appendFileSync(textFile, nyurlparam + '\r\n', 'utf8');
+            fs.appendFileSync(textFile, elephant + '\r\n', 'utf8');
+        }
 
-        // const fileContents = fs.readFileSync(textFile, 'utf8');
+        const fileContents = fs.readFileSync(textFile, 'utf8');
 
-        // res.write(fileContents);
+        res.write("<b>File contents: </b><br/>");
+        res.write("<textarea cols='30' rows='10'>" + fileContents + '</textarea><br/>');
+        res.write("----------" + '<br/>');
+        res.write("<b>Current url params:</b><br/>" + JSON.stringify(query) + '<br/>');
         res.end();
     })
-    .listen(8080);
+    .listen(port);
+
+    console.log("Server ready. http://localhost:" + port);
