75af95af6aabca7ba6f5707df1787b5547405317
Thomas <tbaglioni@hotmail.com>
2019-04-30 21:50:01 +0200

    server with undefined error

--- 04 Fourth session/webserver.js
+++ 04 Fourth session/webserver.js
@@ -2,22 +2,26 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http 
-    .createServer(function (req, res) {
+http.createServer(function (req, res) {
         const query = url.parse(req.url, true).query;
         const content = query.content;
+        console.log(content);
+        const nyurlparam = query.nyurlparam;
+        const elephant = query.elefant;
+
         const textFile = './textFile.txt';
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
 
-        if (content) {
-            fs.appendFileSync(textFile, content + '/r/n', 'utf8');
+        // if (content) {
+        //     fs.appendFileSync(textFile, content + '/r/n', 'utf8');
+        //     fs.appendFileSync(textFile, nyurlparam + '/r/n', 'utf8');
+        //     fs.appendFileSync(textFile, elephant + '/r/n', 'utf8');
+        // }
 
-        }
+        // const fileContents = fs.readFileSync(textFile, 'utf8');
 
-        const fileContents = fs.readFileSync(textFile, 'utf8');
-
-        res.write(fileContents);
+        // res.write(fileContents);
         res.end();
     })
     .listen(8080);
