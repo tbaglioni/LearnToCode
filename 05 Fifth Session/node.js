ef84ffbc3fae597f0fd06ae483f0e0670af8a7fd
Thomas <tbaglioni@hotmail.com>
2019-05-08 20:43:09 +0200

    Have learned about Events in Node.js and NPM now I will go thru too Upload Files :)

--- 05 Fifth Session/node.js
+++ 05 Fifth Session/node.js
@@ -1,9 +1,7 @@
 var http = require('http');
-var fs = require('fs');
+
+//create a server object:
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
